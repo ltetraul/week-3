d13b9c3c3d90794a6c0fbe869390a50bba4ae6fc
ltetraul <143654006+ltetraul@users.noreply.github.com>
2025-09-14 11:17:58 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -1,5 +1,12 @@
 import seaborn as sns
 import pandas as pd
 
-
-# update/add code below ...
+#exercise 1
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+print(fib(10))
